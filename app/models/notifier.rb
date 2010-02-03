2194c6008be7ad9f87b9e519ff27d302dd19809b
Dominik Wei-Fieg <dominik@nono.local>
2010-02-03 10:15:31 +0100

    reset password email subject fixed

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,10 +1,11 @@
 class Notifier < ActionMailer::Base  
   default_url_options[:host] = "www.qifood.eu"
-  def password_reset_instructions(user)  
-    subject       t('user.password_reset_instructions_title')
+  
+  def password_reset_instructions(user, title = I18n.t('user.password_reset_instructions_title') )  
     from          "info@qifood.eu"  
     recipients    user.email  
     sent_on       Time.now  
+    subject       title
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)  
   end  
 end
